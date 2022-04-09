3934f4634b896c0c5d3d0136caa1fd3146c06c33
Eduardo Schenato <eduardosantos59@hotmail.com>
2022-04-09 09:00:30 -0300

    Alteração no texto do login

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
     <body>
         <form action="fazerLogin.php" method="POST">
             <fieldset>
-                <legend>Login</legend>
+                <legend>Entrar no sistema</legend>
                 <label for="login">Login:</label>
                 <input type="text" name="login" id="login" placeholder="Login">
                 <br>
